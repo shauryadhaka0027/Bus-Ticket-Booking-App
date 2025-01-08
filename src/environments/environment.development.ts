f6f19aa7594abecf4819aac9df91efa06a359de3
shaurya <shauryadhaka3@gmail.com>
2025-01-08 12:32:40 +0530

    new url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,5 +4,5 @@ export interface Environment {
   }
 export const environment = {
     production: true,
-    API_URL: 'http://localhost:3000/api',
+    API_URL: 'https://bus-ticket-booking-app-beta.vercel.app/api',
 };
