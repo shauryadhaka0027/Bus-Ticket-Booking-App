df400ee83bc90061e1d165a276570fcbca21beef
Shaurya Dhaka <138813918+shauryadhaka0027@users.noreply.github.com>
2025-01-08 11:59:13 +0530

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export interface Environment {
-    production: boolean;
-    API_URL: string;
-  }
-export const environment : Environment = {
-     production: false,
-    API_URL:"http://localhost:3000/api"
-};
